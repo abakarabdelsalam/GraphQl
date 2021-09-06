da9d7be38878444c46877cca524053df8ba279e0
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-09-06 16:56:01 +0200

    the args

---
+++ src/db/db.mjs
@@ -0,0 +1,17 @@
+export const db = {
+    todos: [
+        {
+            id: 1,
+            name: "Je m'appelle",
+            content: "premier GraphQl",
+            status: "WAINTING"
+        },
+        {
+            id: 2,
+            name: "Tu t'appelle",
+            content: "deuxieme GraphQl",
+            status: "DONE"
+
+        }
+    ]
+}
--- src/resolvers/Query.mjs
+++ src/resolvers/Query.mjs
@@ -1,14 +1,15 @@
-const todos = [
-    {
-        id: 1,
-        name: "Je m'appelle",
-        content: "premier GraphQl",
-        status: "WAINTING"
-    }
-]
+import { db } from "./../db/db.mjs";
+
 export const Query = {
     hello: (_, { name }) => `Hello my ${name || 'World'}`,
     getTodos: () => {
-        return todos;
+        return db.todos;
     },
-}
+    getTodoById: (parent, { id }, context, info) => {
+        const todo = db.todos.find((todo) => todo.id === id);
+        if (!todo) {
+            throw new Error(`Le todo ${id} nexiste pqs`);
+        }
+        return todo;
+    },
+};
